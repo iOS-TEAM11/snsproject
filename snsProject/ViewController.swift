7d1a4f52044e38c73a8df297c2ca4f8dc1627c03
riyeonlee <gjffod100@gmail.com>
2023-08-14 15:10:00 +0900

    test

--- snsProject/ViewController.swift
+++ snsProject/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(1)
         // Do any additional setup after loading the view.
     }
     
