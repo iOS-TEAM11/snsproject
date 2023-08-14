9dad6216d8dc8a1be0c5cdb7e37f88f63bf95728
Luna <eskw0701@naver.com>
2023-08-15 02:42:01 +0900

    [FEAT]: 개인 페이지의 포스팅 피드 구현 (collection view의 image view 등록) 및 섹션 나누기

--- snsProject/MyPage/MyPageViewController.swift
+++ snsProject/MyPage/MyPageViewController.swift
@@ -28,23 +28,53 @@ class MyPageViewController: UIViewController {
             UINib(nibName: "MyPageCollectionViewCell",
                   bundle: nil),
             forCellWithReuseIdentifier: MyPageCollectionViewCell.identifier)
+
+        // postCell 등록
+        myPageCollectionView.register(
+            UINib(nibName: "PostCollectionViewCell",
+                  bundle: nil),
+            forCellWithReuseIdentifier: PostCollectionViewCell.identifier)
     }
 }
 
 extension MyPageViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        1
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
     }
 
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: MyPageCollectionViewCell.identifier,
-            for: indexPath) as? MyPageCollectionViewCell
-        else {
-            // return UICollectionViewCell()
-            fatalError("cell을 불러오지 못하였습니다")
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        default:
+            return 24
         }
-        return cell
+    }
+
+    // cell 생성
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let section = indexPath.section
+
+        switch section {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: MyPageCollectionViewCell.identifier,
+                for: indexPath) as? MyPageCollectionViewCell
+            else {
+                // return UICollectionViewCell()
+                fatalError("cell을 불러오지 못하였습니다")
+            }
+            return cell
+        default:
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: PostCollectionViewCell.identifier,
+                for: indexPath) as? PostCollectionViewCell
+            else {
+                // return UICollectionViewCell()
+                fatalError("cell을 불러오지 못하였습니다")
+            }
+            return cell
+        }
     }
 }
 
@@ -52,8 +82,38 @@ extension MyPageViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize
-    { return CGSize(
-        width: collectionView.frame.width,
-        height: CGFloat(159))
+    {
+        let section = indexPath.section
+        switch section {
+        case 0:
+            return CGSize(
+                width: collectionView.frame.width,
+                height: CGFloat(159))
+        default:
+            let side = CGFloat((collectionView.frame.width / 3) - (4/3)) // 하나의 면의 크기를 같게 하기위해
+            return CGSize(
+                width: side,
+                height: side)
+        }
+    }
+
+    // 행
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return CGFloat(0)
+        default:
+            return CGFloat(1)
+        }
+    }
+
+    // 열
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return CGFloat(0)
+        default:
+            return CGFloat(1)
+        }
     }
 }
