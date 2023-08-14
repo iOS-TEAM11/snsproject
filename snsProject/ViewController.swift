4ff687875eed88c0b9a1557f0a015c0fb3074db4
se-ryeong <seryz029@gmail.com>
2023-08-14 12:58:51 +0900

    initial setting

---
+++ snsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snsProject
+//
+//  Created by t2023-m0062 on 2023/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
