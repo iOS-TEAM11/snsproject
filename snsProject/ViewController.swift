eab36fc6fb4df6999694247e3404f8ba3ea1d55f
suojae <suojae3@gmail.com>
2023-08-14 14:52:35 +0900

    test

--- snsProject/ViewController.swift
+++ snsProject/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    print("git test")
+    
+    var test = "test"
 }
 
