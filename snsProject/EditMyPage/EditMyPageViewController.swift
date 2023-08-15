6a75765cf5fc16be5809a9ef652d7c750f9a2de3
HAHOHAHOL <ghgud94@gmail.com>
2023-08-15 20:51:55 +0900

    [FEAT]: 페이지 추가, 이미지,상단 버튼 추가

---
+++ snsProject/EditMyPage/EditMyPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EditMyPageViewController.swift
+//  snsProject
+//
+//  Created by 하호형 on 2023/08/14.
+//
+// ***********    푸시 전 인포 변경    ************
+import UIKit
+
+class EditMyPageViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+                //이미지 버튼 원 작업
+        profileImage.layer.cornerRadius = 0.5 * profileImage.bounds.size.width
+        profileImage.clipsToBounds = true
+    }
+}
+    
