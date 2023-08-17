c71424e7eed3eb4b80a0bd4012a0954b0c878715
suojae <suojae3@gmail.com>
2023-08-17 22:08:28 +0900

    [FEAT] EditMyPage 데이터 주입

--- snsProject/Data.swift
+++ snsProject/Data.swift
@@ -1,7 +1,20 @@
 
 import UIKit
 
+// 게시물 저장 데이터
+
 struct Post {
     let image: UIImage 
     let description: String
+}
+
+
+// 유저 데이터
+struct UserDatum {
+    let userImage: UIImage
+    let userName: String
+    let userNickName: String
+    let userGebder: Bool
+    let userIntro: String
+    let userLink: String
 }
--- snsProject/EditMyPage/EditMyPageViewController.swift
+++ snsProject/EditMyPage/EditMyPageViewController.swift
@@ -9,17 +9,83 @@ import UIKit
 
 class EditMyPageViewController: UIViewController{
     
+    var userName: [String] = []
+    var userNickName: [String] = []
+    var userGender: [String] = []
+    var userIntro: [String] = []
+    var userLink: [String] = []
+    
     @IBOutlet weak var profileImage: UIButton!
+
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfNickName: UITextField!
+    @IBOutlet weak var tfIntro: UITextField!
+    @IBOutlet weak var tfLink: UITextField!
+    @IBOutlet weak var tfGender: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .black
-        
+                
         //이미지 버튼 원 작업
         profileImage.layer.cornerRadius = 0.5 * profileImage.bounds.size.width
         profileImage.clipsToBounds = true
+    }
+    
+   
+    @IBAction func enterUserData(_ sender: Any) {
+    
+    
+        // Name
+        if let textField = tfName, let name = textField.text, !name.isEmpty {
+            userName.append(name)
+        } else {
+            showAlert(message: "이름을 다시 입력해주세요")
+            return
+        }
+
+        // Nickname
+        if let textField = tfNickName, let nickname = textField.text, !nickname.isEmpty {
+            userNickName.append(nickname)
+        } else {
+            showAlert(message: "닉네임을 다시 입력해주세요")
+            return
+        }
+
+        // Intro
+        if let textField = tfIntro, let intro = textField.text, !intro.isEmpty {
+            userIntro.append(intro)
+        } else {
+            showAlert(message: "소개글을 다시 입력해주세요")
+            return
+        }
+
+        // Link
+        if let textField = tfLink, let link = textField.text, !link.isEmpty {
+            userLink.append(link)
+        } else {
+            showAlert(message: "링크를 다시 입력해주세요")
+            return
+        }
+
+        // Gender
+        if let textField = tfGender, let gender = textField.text, !gender.isEmpty {
+            userGender.append(gender)
+        } else {
+            showAlert(message: "성별을 다시 입력해주세요")
+            return
+        }
+
+        
+
+        
+        
+        func showAlert(message: String) {
+            let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     
 }
